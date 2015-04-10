bc2aee65bf486691f25081e2ca2c319a4e18af33
Scott Maddox <smaddox@utexas.edu>
2015-04-09 19:28:07 -0500

    simplify `examples.pn_diode`

--- src/examples/pn_diode.py
+++ src/examples/pn_diode.py
@@ -23,22 +23,14 @@ import os
 import sys
 sys.path.insert(0,
     os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-from obpds import GaAs, cm3, um, Material, Layer, OhmicContact, LayerStructure
+from obpds import *
 
 # Layers
 p = Layer(1*um, Material(GaAs,  1e17/cm3))
 n = Layer(1*um, Material(GaAs, -1e17/cm3))
 
-# Contacts
-top = OhmicContact()
-bottom = OhmicContact()
-
-# Layer Structure
-ls = LayerStructure([top, p, n, bottom])
-
-# ls.show_composition() # show the composition vs. depth
-# ls.show_doping() # show the doping vs. depth
-# ls.show_flatband() # show the flatband profile vs. depth
+# Device
+d = Device(layers=[p,n])
 
 # Simulate and show the equilibrium band profile using the default method.
-ls.show_equilibrium()
+d.show_equilibrium()
