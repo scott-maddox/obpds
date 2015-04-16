bde978248e395afb076e625fc5eba42ca08da033
Scott Maddox <smaddox@utexas.edu>
2015-04-15 20:46:52 -0500

    simplify `Layer` and add `CompoundLayer`

--- src/obpds/layer.py
+++ src/obpds/layer.py
@@ -27,29 +27,39 @@ class Layer(object):
         ------
         thickness : float
             thickness (cm)
-        material : openbandparams material
-            the material in the layer
+        material : Material
+            material the layer is composed of
         '''
-        self.thickness = to_units(thickness, cm)
-        self.material = material
+        self._thickness = to_units(thickness, cm)
+        self._material = material
 
     def get_thickness(self):
         '''
         Returns the layer thickness (cm).
         '''
-        return self.thickness
+        return self._thickness
 
-    def write_AMBER_recipe(self, fobj):
-        fobj.write('l {} {:.3f} ! Angs\n'.format(self.material.name,
-                                                 self.thickness*1e8))
+    def get_material(self, x):
+        '''
+        Returns the material at a given position (cm) within the layer.
+        
+        Parameters
+        ----------
+        x : float
+            position (cm) between zero and the layer thickness
+        '''
+        if x < 0. or x > self.get_thickness():
+            raise ValueError('x not within range [{:g}, {:g}]'
+                             ''.format(0., self.get_thickness()))
+        return self._material
 
-    def get_flatband(self, T=300):
+    def get_flatband(self, T=300.):
         '''
         returns x, Ev, Ec, Ei
         
         x will be [0, thickness]
         Ev will be [VBO, VBO]
-        Ec will be [VBO+Eg, VBO+Eg]
+        Ec will be [CBO, CBO]
         Ei will be [VBO+Ei, VBO+Ei]
         
         Arguments
@@ -57,12 +67,100 @@ class Layer(object):
         T : float
             the temperature
         '''
-        x = [0, self.get_thickness()]
-        Ev = [self.material.VBO(T=T),
-              self.material.VBO(T=T)]
-        Ec = [self.material.VBO(T=T)+self.material.Eg(T=T),
-              self.material.VBO(T=T)+self.material.Eg(T=T)]
-        Ei = [self.material.VBO(T=T)+self.material.Ei(T=T),
-              self.material.VBO(T=T)+self.material.Ei(T=T)]
-        return x, Ev, Ec, Ei
+        x = (0., self.get_thickness())
+        VBO = self._material.VBO(T=T)
+        CBO = self._material.CBO(T=T)
+        ILO = VBO + self.material.Ei(T=T)
+        return x, (VBO, VBO), (CBO, CBO), (ILO, ILO)
+
+    def write_AMBER_recipe(self, fobj):
+        fobj.write('l {} {:.3f} ! Angs\n'.format(self._material.name,
+                                                 self._thickness*1e8))
+
+class CompoundLayer(Layer):
+    
+    def __init__(self, layers):
+        self._layers = []
+        if len(layers) < 1:
+            raise ValueError('There must be at least one layer.')
+        else:
+            for layer in layers:
+                self.append(layer)
+
+    def __iter__(self):
+        return self._layers.__iter__()
+    
+    def append(self, layer):
+        '''
+        Append a layer on the right/bottom.
+        '''
+        if not isinstance(layer, Layer):
+            raise TypeError('Layers must be an instance of '
+                            'the `Layer` class.')
+        self._layers.append(layer)
+    
+    def insert(self, index, layer):
+        '''
+        Insert a layer at the given index.
+        '''
+        if not isinstance(layer, Layer):
+            raise TypeError('Layers must be an instance of '
+                            'the `Layer` class.')
+        self._layers.insert(index, layer)
+
+    def get_thickness(self):
+        '''
+        Returns the layer thickness (cm).
+        '''
+        sum = 0
+        for layer in self:
+            sum += layer.get_thickness()
+        return sum
+
+    def get_material(self, x):
+        '''
+        Returns the material at a given position (cm) within the layer.
         
+        Parameters
+        ----------
+        x : float
+            position (cm) between zero and the layer thickness
+        '''
+        if x < 0. or x > self.get_thickness():
+            raise ValueError('x not within range [{:g}, {:g}]'
+                             ''.format(0., self.get_thickness()))
+        last_x = 0
+        for layer in self:
+            next_x = last_x + layer.get_thickness()
+            if x <= next_x:
+                return layer.get_material(x-last_x)
+            last_x = next_x
+        else:
+            raise RuntimeError('unexpected execution path')
+
+    def get_flatband(self, T=300.):
+        '''
+        returns x, Ev, Ec, Ei
+        
+        x will be [0, ..., thickness]
+        Ev will be [VBO, ..., VBO]
+        Ec will be [CBO, ..., CBO]
+        Ei will be [VBO+Ei, ..., VBO+Ei]
+        
+        Arguments
+        ---------
+        T : float
+            the temperature
+        '''
+        x, Ev, Ec, Ei = [], [], [], []
+        last_x = 0
+        for layer in self:
+            l_x, l_Ev, l_Ec, l_Ei = layer.get_flatband(T=T)
+            for layer_x in l_x:
+                x.append(last_x + layer_x)
+            Ev.extend(l_Ev)
+            Ec.extend(l_Ec)
+            Ei.extend(l_Ei)
+            thickness = l_x[-1]
+            last_x += thickness
+        return x, Ev, Ec, Ei
