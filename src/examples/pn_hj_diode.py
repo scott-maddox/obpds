f8749edb595a33ef5fe420f5594ed8da4110a91a
Scott Maddox <smaddox@utexas.edu>
2015-04-15 20:48:46 -0500

    update `examples.pn_hj_diode`

--- src/examples/pn_hj_diode.py
+++ src/examples/pn_hj_diode.py
@@ -23,22 +23,14 @@ import os
 import sys
 sys.path.insert(0,
     os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-from obpds import GaAs, AlGaAs, cm3, um, Material, Layer, OhmicContact, LayerStructure
+from obpds import *
 
 # Layers
 p = Layer(1*um, Material(GaAs,  1e17/cm3))
 N = Layer(1*um, Material(AlGaAs(Al=0.3), -1e17/cm3))
 
-# Contacts
-top = OhmicContact()
-bottom = OhmicContact()
-
-# Layer Structure
-ls = LayerStructure([top, p, N, bottom])
-
-# ls.show_composition() # show the composition vs. depth
-# ls.show_doping() # show the doping vs. depth
-# ls.show_flatband() # show the flatband profile vs. depth
+# Device
+d = TwoTerminalDevice(layers=[p, N])
 
 # Simulate and show the equilibrium band profile using the default method.
-ls.show_equilibrium()
+d.show_equilibrium()
