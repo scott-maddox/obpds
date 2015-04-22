7f3a5775235c64f0b91ab6c722398544db46d233
Scott Maddox <smaddox@utexas.edu>
2015-04-22 06:51:50 -0500

    add non-parabolicity to `examples/degenerate_pn_diode.py`

--- src/examples/degenerate_pn_diode.py
+++ src/examples/degenerate_pn_diode.py
@@ -39,15 +39,20 @@ ax1.set_ymargin(0.05)
 ax1.set_ylabel('Energy (eV)')
 ax1.set_xlabel('Depth (nm)')
 
-solution = d.get_equilibrium(boltz=True)
+solution = d.get_equilibrium(approx='boltzmann')
+x = solution.x*1e7 # nm
+ax1.plot(x, solution.Ev, 'r:')
+ax1.plot(x, solution.Ec, 'b:', lw=2, label='Parabolic Boltzmann')
+
+solution = d.get_equilibrium(approx='parabolic')
 x = solution.x*1e7 # nm
 ax1.plot(x, solution.Ev, 'r--')
-ax1.plot(x, solution.Ec, 'b--', label='Boltzmann')
+ax1.plot(x, solution.Ec, 'b--', lw=2, label='Parabolic Fermi-Dirac')
 
-solution = d.get_equilibrium(boltz=False)
+solution = d.get_equilibrium(approx=None)
 x = solution.x*1e7 # nm
 ax1.plot(x, solution.Ev, 'r-')
-ax1.plot(x, solution.Ec, 'b-', label='Fermi-Dirac')
+ax1.plot(x, solution.Ec, 'b-', label='Non-parabolic Fermi-Dirac')
 ax1.plot(x, solution.Ef, 'k--')
 
 ax1.legend(loc='best')
