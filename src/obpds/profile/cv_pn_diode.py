8db84720fb3c44e2ec42d241a9d12b660e457ea8
Scott Maddox <smaddox@utexas.edu>
2015-05-07 16:50:19 -0500

    Add `profile` package

---
+++ src/obpds/profile/cv_pn_diode.py
@@ -0,0 +1,39 @@
+#
+#   Copyright (c) 2015, Scott J Maddox
+#
+#   This file is part of Open Band Parameters Device Simulator (OBPDS).
+#
+#   OBPDS is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   OBPDS is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with OBPDS.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+# Make sure we import the local obpds version
+import os
+import sys
+sys.path.insert(0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from obpds import *
+
+# Layers
+p = Layer(0.5*um, Material(GaAs,  1e18/cm3))
+n = Layer(1*um, Material(GaAs, -1e16/cm3))
+
+# Device
+d = TwoTerminalDevice(layers=[p, n],
+                      Fp='left',
+                      Fn='right')
+
+import cProfile
+cProfile.run(statement='d.get_cv(-2, 2)', filename='stats')
+print 'Done.'
